61ba11dfdd9b26ff657bc537e69eeb121fde2f06
kol-oss <kolyakhozhajinov@gmail.com>
2024-12-12 22:27:52 +0200

    Add basic annotations

---
+++ laboratory-work-2-advanceed/src/main/java/edu/kpi/reflection/annotations/Column.java
@@ -0,0 +1,16 @@
+package edu.kpi.reflection.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Column {
+    String name() default "";
+    boolean unique() default false;
+
+    boolean nullable() default true;
+    boolean updatable() default true;
+}
---
+++ laboratory-work-2-advanceed/src/main/java/edu/kpi/reflection/builder/Entity.java
@@ -0,0 +1,8 @@
+package edu.kpi.reflection.builder;
+
+public interface Entity {
+    String insert();
+    String select();
+    String update();
+    String delete();
+}
